27b1eb66930157613309bd48b7cb8601628ce60d
Anand Mohan Sharma <anandmsharma786@gmail.com>
2024-03-13 13:09:37 +0530

    Book Management using ngrx

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BookReducer } from './books/book.reducers';
+import { BookListComponent } from './book-list/book-list.component';
+import { AppState } from './app.state';
+import { EffectsModule } from '@ngrx/effects';
+import { BookEffects } from './books/book.effect';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BookListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    StoreModule.forRoot<AppState>({book: BookReducer}),
+    EffectsModule.forRoot([BookEffects]),
+    StoreDevtoolsModule.instrument()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
